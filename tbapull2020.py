75a1ea8fc091395bbf0796c66198a596e6eaca45
Kristen Chong <kristen@highcurrent.io>
2020-04-30 04:40:58 +0000

    Updating for the end of the 2020 season

--- tbapull2020.py
+++ tbapull2020.py
@@ -6,19 +6,20 @@ import mysql.connector
 import json
 import re
 import logging
+import decimal
 
 API_KEY = '4ZhSjJb0eE1Fhkw5MJ80Dscvn8bGiFI93XQBPrzl4RtWPxJuJZZWuUYqNmoBFnDa'
 BASE_URL = 'https://www.thebluealliance.com/api/v3'
 scoutingdb = mysql.connector.connect(
 	host='localhost',
-	user='serveradmin',
-	passwd='askmewhyiplatypi',
+	user='root',
+	passwd='KikoTheDerpyCat',
 	database='scouting2020'
 )
 sqlsession = FuturesSession()
 sqlsession.headers.update({'X-TBA-Auth-Key':API_KEY})
 
-stdev = 17.1
+stdev = decimal.Decimal(58.3)
 
 dbcursor = scoutingdb.cursor()
 
@@ -46,10 +47,10 @@ def matchDataPull():
 	futures = list()
 	for x in eventlist:
 		future = sqlsession.get(BASE_URL + '/event/' + x[0] + '/matches/simple', headers={'If-Modified-Since':x[1]})
-		logging.info("%s was last modified %s", (x[0], x[1]))
+		logging.debug("%s was last modified %s", x[0], x[1])
 		futures.append(future)
 	vals = list()
-	sql = "replace into mastermatchlist (matchkey, eventkey, scheduledtime, playedtime, red1, red2, red3, blue1, blue2, blue3, redscore, bluescore, predictedredmargin, probredwins, actualmargin, complevel) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+	sql = "insert into mastermatchlist (matchkey, eventkey, scheduledtime, playedtime, red1, red2, red3, blue1, blue2, blue3, redscore, bluescore, actualmargin, complevel) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) on duplicate key update playedtime=values(playedtime), redscore=values(redscore), bluescore=values(bluescore), actualmargin=values(actualmargin)"
 	for future in futures:
 		reply = future.result()
 		if reply.status_code == 200:
@@ -61,16 +62,6 @@ def matchDataPull():
 			updatedevents = updatedevents + 1
 			scoutingdb.commit()
 			for y in matchdata:
-				red1elo = lookupTeamELO(y["alliances"]["red"]["team_keys"][0]) # red1
-				red2elo = lookupTeamELO(y["alliances"]["red"]["team_keys"][1]) # red2
-				red3elo = lookupTeamELO(y["alliances"]["red"]["team_keys"][2]) # red3
-				blue1elo = lookupTeamELO(y["alliances"]["blue"]["team_keys"][0]) # blue1
-				blue2elo = lookupTeamELO(y["alliances"]["blue"]["team_keys"][1]) # blue2
-				blue3elo = lookupTeamELO(y["alliances"]["blue"]["team_keys"][2]) # blue3
-				sumredelo = red1elo + red2elo + red3elo
-				sumblueelo = blue1elo + blue2elo + blue3elo
-				predictedredmargin = (sumredelo - sumblueelo)*0.004*stdev
-				redwinchance = 1/(pow(10,(sumblueelo - sumredelo)/400)+1)
 				vals.append((
 					y["key"],
 					thiseventkey,
@@ -84,8 +75,6 @@ def matchDataPull():
 					int(re.sub(r"\D", "", y["alliances"]["blue"]["team_keys"][2])),
 					y["alliances"]["red"]["score"],
 					y["alliances"]["blue"]["score"],
-					predictedredmargin,
-					redwinchance,
 					y["alliances"]["red"]["score"] - y["alliances"]["blue"]["score"],
 					y["comp_level"]))
 		dbcursor.executemany(sql, vals)
@@ -106,41 +95,46 @@ def resetELOs():
 	#re-populate all ELOs to the start-of-season values
 
 def calcELOs():
-	resetELOs()
+	resetELOs()			# commented out to enable incremental calculations only.
 	logging.info("Elo table reset!")
-	dbcursor.execute("select matchkey, red1, red2, red3, blue1, blue2, blue3, redscore, bluescore, predictedredmargin, actualmargin, complevel from mastermatchlist where playedtime is not null order by playedtime")
+	dbcursor.execute("select matchkey, red1, red2, red3, blue1, blue2, blue3, redscore, bluescore, actualmargin, complevel from mastermatchlist order by scheduledtime")
 	resultdata = dbcursor.fetchall()
 	sql1 = "update teamEloList set currentelo = %s where teamnumber = %s"
 	for x in resultdata:
-		
 		red1elo = lookupTeamELO(x[1]) # red1
 		red2elo = lookupTeamELO(x[2]) # red2
 		red3elo = lookupTeamELO(x[3]) # red3
 		blue1elo = lookupTeamELO(x[4]) # blue1
 		blue2elo = lookupTeamELO(x[5]) # blue2
 		blue3elo = lookupTeamELO(x[6]) # blue3
-		kval = 12.0
+		sumredelo = decimal.Decimal(red1elo + red2elo + red3elo)
+		sumblueelo = decimal.Decimal(blue1elo + blue2elo + blue3elo)
+		predictedredmargin = decimal.Decimal(sumredelo - sumblueelo)*decimal.Decimal(0.004)*stdev
+		redwinchance = decimal.Decimal(1/(pow(10,(sumblueelo - sumredelo)/400)+1))
+		kval = decimal.Decimal(12.0)
 		if x[10] != 'qm': # complevel
-			kval = 3.0
-		elochange = (x[10] - x[9])/stdev*kval # predictedredmargin & actualmargin
+			kval = decimal.Decimal(3.0)
+		elochange = kval*decimal.Decimal(x[9] - predictedredmargin)/stdev # predictedredmargin & actualmargin
 		values1 = [
 			(red1elo + elochange, x[1]),
 			(red2elo + elochange, x[2]),
 			(red3elo + elochange, x[3]),
-			(blue1elo + elochange, x[4]),
-			(blue2elo + elochange, x[5]),
-			(blue3elo + elochange, x[6])
+			(blue1elo - elochange, x[4]),
+			(blue2elo - elochange, x[5]),
+			(blue3elo - elochange, x[6])
 		]
-		dbcursor.executemany(sql1,tuple(values1))
+		if x[9] != 0:
+			dbcursor.executemany(sql1,tuple(values1))
+		dbcursor.execute("update mastermatchlist set predictedredmargin = %s where matchkey = %s", (predictedredmargin, x[0]))
+		dbcursor.execute("update mastermatchlist set probredwins = %s where matchkey = %s", (redwinchance, x[0]))
 		scoutingdb.commit()
-		logging.info("Elos updated for teams in match " + x[0] + "by change amount " + elochange)
+		logging.debug("Elos updated for teams in match " + x[0] + "by change amount " + str(elochange))
 	#perfrorm all elo calculations for full match list and udpate
 
 def lookupTeamELO(teamnumber):
-	dbcursor.execute("select * from teamEloList")
-	teamelos = dbcursor.fetchall()
-	result = [entry for entry in teamelos if entry[0] == teamnumber]
-	return result[0][1]	
+	dbcursor.execute("select currentelo from teamEloList where teamnumber = %s", (teamnumber,))
+	teamelo = dbcursor.fetchall()
+	return decimal.Decimal(teamelo[0][0])
 
 def main():
 	logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='tba-pull.log', level=logging.INFO)
